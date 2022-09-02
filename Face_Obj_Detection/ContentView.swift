f00add504eeeff332c6c52ccc2e1c4af6db69df1
Rohan Kumar <rohankumar@ucsb.edu>
2022-09-02 00:25:02 -0700

    Initial Commit

---
+++ Face_Obj_Detection/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Face_Obj_Detection
+//
+//  Created by Rohan Kumar on 9/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Face_Obj_Detection/Face_Obj_DetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Face_Obj_DetectionApp.swift
+//  Face_Obj_Detection
+//
+//  Created by Rohan Kumar on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Face_Obj_DetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
